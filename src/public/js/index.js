6fa1f87f93ae3ef41c091a6b3b027427cc3c8ca0
Nicolás Arévalo <inag.458@gmail.com>
2022-11-11 22:08:31 -0500

    Cambiando cosas a ver si aleatoriamente funciona

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -5,12 +5,9 @@ const enviarMensaje = () => {
         alert('Mano, qué vas a mandar? le das a enviar pero está vacío el mensaje, telepatico no soy.')
         return
     }
-
     const minutes = new Date().getHours()
     let seconds = new Date().getMinutes()
-
     if( seconds < 10 ) seconds = `0${seconds}`
-    
     socket.volatile.emit("sent message", { message: mssg.value, time: `${minutes}:${seconds}` })
     mssg.value = ""
 }
@@ -18,14 +15,10 @@ document.addEventListener('keypress', ({ key }) => {
     if (key === "Enter") enviarMensaje()
 })
 sendMessage.addEventListener("click", enviarMensaje)
-
 socket.on("sent message", ({ user, message, time, id }) => {
-
     let newMessage
-
     if (socket.id === id) {
         newMessage = document.createRange().createContextualFragment(`
-
             <div class="message my-mssg" id="mssgBubble" key=${id}>
                 <header>
                     <span id="username">${user}</span>
@@ -36,10 +29,8 @@ socket.on("sent message", ({ user, message, time, id }) => {
                 </p>
             </div>
         `)
-
     } else {
         newMessage = document.createRange().createContextualFragment(`
-
             <div class="message" id="mssgBubble" key=${id}>
                 <header>
                     <span id="username">${user}</span>
@@ -50,14 +41,10 @@ socket.on("sent message", ({ user, message, time, id }) => {
                 </p>
             </div>
         `)
-
     }
-
-
     allMssgs.append(newMessage)
     document.querySelector('main').scrollTop = allMssgs.scrollHeight
 })
-
 socket.on("new connection", connections => {
     countConectados.textContent = connections
 })
